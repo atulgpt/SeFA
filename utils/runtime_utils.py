41b4eab1b0207e28a66d5da59f7fe1146d107bda
Atul Gupta <atlgpt17@gmail.com>
2026-08-20 15:35:21 +0530

    Update the message in `warn_missing_module`
    Display install instruction to install deps from `pyproject.toml`

--- utils/runtime_utils.py
+++ utils/runtime_utils.py
@@ -8,5 +8,5 @@ def warn_missing_module(package: str) -> t.Optional[types.ModuleType]:
         return __import__(package)
     except ImportError:
         print(f'You don\'t have "{package}" installed to run the script.\
- Please run: "python3 -m pip install {package}"')
+ Please run: "python -m pip install -e ."')
         sys.exit(0)
